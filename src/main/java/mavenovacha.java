deed284e72bcf37163ee531dcecd3f4ff9dcdf3f
Nenad <nresimic@yahoo.com>
2019-06-28 11:46:11 +0200

    Second commit

--- src/main/java/mavenovacha.java
+++ src/main/java/mavenovacha.java
@@ -2,14 +2,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class mavenovacha {
-        public static void main(String[] args) {
-            
-            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    public static void main(String[] args) {
 
-            WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", ".src/main/java/driver/chromedriver.exe");
 
-            driver.get("https://www.google.com");
+        WebDriver driver = new ChromeDriver();
 
-            //Closes chrome browser
-            driver.quit();
+        driver.get("https://www.google.com");
+
+        //Closes chrome browser
+        driver.quit();
+    }
 }
