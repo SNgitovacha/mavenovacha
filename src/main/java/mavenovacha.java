6cfa80049656a8bd127150eea90cc7e54a9edf9d
Nenad <nresimic@yahoo.com>
2019-06-28 11:41:06 +0200

    First Commit

---
+++ src/main/java/mavenovacha.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class mavenovacha {
+        public static void main(String[] args) {
+            
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+
+            WebDriver driver = new ChromeDriver();
+
+            driver.get("https://www.google.com");
+
+            //Closes chrome browser
+            driver.quit();
+}
