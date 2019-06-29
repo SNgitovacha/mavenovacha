51ca63e894596504ed6f96b0dcd018a75fbbbf22
Nenad <nresimic@yahoo.com>
2019-06-29 17:09:07 +0200

    Sa Savetom

--- src/main/java/mavenovacha.java
+++ src/main/java/mavenovacha.java
@@ -14,6 +14,7 @@ public class mavenovacha {
         google.sendKeys("testing");
         google.sendKeys(Keys.RETURN);
         //Closes chrome browser
+        //Saveta
         //driver.quit();
     }
 }
