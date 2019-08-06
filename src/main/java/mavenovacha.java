3ab65af1064890d7af4b5bdc4598e92300274d3b
Nenad Resimic <nenad.resimic@tnation.eu>
2019-08-06 11:29:15 +0200

    newchanges

--- src/main/java/mavenovacha.java
+++ src/main/java/mavenovacha.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Keys;
 public class mavenovacha {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "./src/main/java/driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         WebElement google = driver.findElement(By.className("gLFyf"));
