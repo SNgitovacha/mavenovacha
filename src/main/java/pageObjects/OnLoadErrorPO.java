27fb09740d505efe5465d51cacdc264094f912f4
Saveta Peric <saveta.peric@tnation.eu>
2019-08-28 10:29:37 +0200

    OnloadError Savke pokidala, kaze Nesha ne hvalim se ja :)

--- src/main/java/pageObjects/OnLoadErrorPO.java
+++ src/main/java/pageObjects/OnLoadErrorPO.java
@@ -1,11 +1,37 @@
 package pageObjects;
 
 import logic.BasePage;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Date;
+import java.util.logging.Level;
+
 public class OnLoadErrorPO extends BasePage {
+
     public OnLoadErrorPO(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
+    }
+//    public void catchJavascriptError(){
+//        ((JavascriptExecutor) driver).executeScript("return window.javascript_errors");
+//    }
+
+    public void ExtractJSLogs() {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        LoggingPreferences loggingprefs = new LoggingPreferences();
+        loggingprefs.enable(LogType.BROWSER,Level.ALL);
+        capabilities.setCapability(CapabilityType.LOGGING_PREFS,loggingprefs);
+
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        for (LogEntry entry : logEntries) {
+            System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+        }
     }
 }
--- src/test/java/tests/OnloadError.java
+++ src/test/java/tests/OnloadError.java
@@ -5,23 +5,27 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
+import pageObjects.OnLoadErrorPO;
 
 import static utils.Variables.*;
 
 public class OnloadError {
 
     HomePage homePage;
+    OnLoadErrorPO onloadError;
 
     @BeforeClass(alwaysRun = true)
     public void SetUp(){
         Browser.initializeDriver();
         Browser.openURL("http://the-internet.herokuapp.com/");
         homePage = new HomePage(Browser.driver, Browser.wait);
+        onloadError = new OnLoadErrorPO(Browser.driver, Browser.wait);
     }
 
     @Test
     public void testOnloadError(){
         homePage.clickOnLink(ONLOADERROR_LINK);
+        onloadError.ExtractJSLogs();
     }
 
 //    @AfterClass(alwaysRun = true)
